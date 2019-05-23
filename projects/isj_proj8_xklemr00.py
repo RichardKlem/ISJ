bd44d6df2de77c4f89326732f3bfb490e961b809
Richard Klem <48335467+RichardKlem@users.noreply.github.com>
2019-05-23 15:00:52 +0200

    Add files via upload

---
+++ projects/isj_proj8_xklemr00.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def first_with_given_key(iterable, key=lambda x: x):
+    """
+    Funkce aplikuje klíč na položky objektu iterable a vybírá (generuje) pouze ty, jejichž klíč se dosud nevyskytl.
+    :param iterable: povinný parametr iterable, odpovídající předanému iterovatelnému objektu (může být i nekonečný)
+    :param key: nepovinný parametr key, odpovídající funkci, která při volání na položce objektu iterable vrátí hodnotu klíče,
+        s defaultní hodnotou identické funkce (tedy vrácení přímo položky, na které je funkce zavolána)
+    :return: generuje polozky, jejichz klic se jeste nevyskytl, cini tak pomoci yield
+    """
+
+    iter_key_values = []
+
+    for iter_obj in iterable:
+        if key(iter_obj) not in iter_key_values:
+            iter_key_values.append(key(iter_obj))
+            yield iter_obj
