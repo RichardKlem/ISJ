1873ef57c5a4518294d40a837da7748e525d28de
Richard Klem <richard.klem@seznam.cz>
2021-06-28 23:06:02 +0200

    Break lines

--- projects/isj_proj8_xklemr00.py
+++ projects/isj_proj8_xklemr00.py
@@ -3,10 +3,13 @@
 
 def first_with_given_key(iterable, key=lambda x: x):
     """
-    Funkce aplikuje klíč na položky objektu iterable a vybírá (generuje) pouze ty, jejichž klíč se dosud nevyskytl.
-    :param iterable: povinný parametr iterable, odpovídající předanému iterovatelnému objektu (může být i nekonečný)
-    :param key: nepovinný parametr key, odpovídající funkci, která při volání na položce objektu iterable vrátí hodnotu klíče,
-        s defaultní hodnotou identické funkce (tedy vrácení přímo položky, na které je funkce zavolána)
+    Funkce aplikuje klíč na položky objektu iterable a vybírá (generuje) pouze ty, jejichž klíč se
+        dosud nevyskytl.
+    :param iterable: povinný parametr iterable, odpovídající předanému iterovatelnému objektu (může
+        být i nekonečný)
+    :param key: nepovinný parametr key, odpovídající funkci, která při volání na položce objektu
+        iterable vrátí hodnotu klíče, s defaultní hodnotou identické funkce (tedy vrácení přímo
+        položky, na které je funkce zavolána)
     :return: generuje polozky, jejichz klic se jeste nevyskytl, cini tak pomoci yield
     """
 
